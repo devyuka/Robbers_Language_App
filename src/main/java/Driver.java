d0b4f8f23e0745b553d8f691f4c7f88218fec9ba
Yuka <yuka.0531.a@gmail.com>
2019-11-11 18:57:40 -0800

    add test examples

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,5 +1,8 @@
 public class Driver {
     public static void main(String[] args) {
         RobbersLanguage test = new RobbersLanguage();
+        System.out.println(test.replaceLetters("joy"));
+        System.out.println(test.replaceLetters("ham"));
+        System.out.println(test.replaceLetters("aieou"));
     }
 }
